abb96b416e8ce5ef23e03b04d8df0b4df8af53d0
jmcob <jmcob@pm.me>
2021-09-30 11:48:24 +0200

    cart temporaire added

--- js/product.js
+++ js/product.js
@@ -38,3 +38,48 @@ let cardsFetch = function () {
     });
 };
 cardsFetch();
+
+//////////
+// cart //
+//////////
+// HTML element : button add to cart
+const toCartBtn = document.getElementById("addToCart");
+
+// fonction getCart pour aller chercher le cart et le stocker dans let cartItems
+function getCart() {
+  let itemsCart = [];
+  console.log("0 " + itemsCart);
+  if (localStorage.getItem("cart") != null) {
+    itemsCart = JSON.parse(localStorage.getItem("cart"));
+    console.log("1 " + itemsCart);
+  }
+  return itemsCart;
+}
+
+// fonction addCart pour ajouter une valeur au cart en passant par let cartItems
+function addCart(idTeddy) {
+  let itemsCart = getCart();
+  console.log("2 " + itemsCart);
+  if (itemsCart.length == 0) {
+    itemsCart[idTeddy] = 1;
+    console.log("2.5 " + itemsCart);
+  } else {
+    let blnTrouve = false;
+    for (let i = 0; i < itemsCart.length; i++) {
+      if (itemsCart.find(idTeddy)) {
+        itemsCart[idTeddy]++;
+        blnTrouve = true;
+      }
+    }
+    if (blnTrouve == false) {
+      itemsCart[idTeddy] = 1;
+    }
+  }
+  localStorage.setItem("cart", JSON.stringify(itemsCart));
+  console.log("3 " + itemsCart);
+}
+
+// au bouton toCartBtn, fonction addCart
+toCartBtn.addEventListener("click", () => {
+  addCart(id);
+});
